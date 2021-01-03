10b3f330b7452f2e2801fb25d060f9cbdb3d26cc
Aubrox <43bast@gmail.com>
2021-01-03 17:50:13 +0000

    add fct tri

--- test.h
+++ test.h
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 
+typedef struct
+{
+    int id;
+    char nom [20];
+    char type [20];
+    int nbExemplaire;
+}Jeux;
+
+void test1(void);
+int remplirTab(Jeux *tabJeux[],int taillePhyJeux);
+void afficherJeux( Jeux *tabJeux[], int tailleLogJeux);
+Jeux LireJeux (FILE *flot);
 int choixMenu(void);
 void affichageMenu(void);
+void triTabJeux(Jeux *tabJeux[], int tailleLogJeux);
+Jeux* copyJeu(Jeux *Jeu);
