4b5ad9d82a304abd233cb824bf2cda3e823e7fc6
Aubrox <76773977+Aubrox@users.noreply.github.com>
2021-01-03 17:22:52 +0000

    PR affichage jeu to master  (#1)
    * add fct affichage jeux
    
    * Delete hello.exe
    
    * Delete test.exe
    
    * add fct tri
    
    * renommage
    
    * git ignore add
    
    * Delete a.exe

--- test.c
+++ test.c
@@ -1,5 +1,14 @@
 #include "test.h"
 
+void globalJeu(void)
+{    
+    int tailleLogJeux;
+    Jeux *tabJeux[50];
+    tailleLogJeux=remplirTab(tabJeux,50);
+    triTabJeux(tabJeux,tailleLogJeux);
+    afficherJeux(tabJeux,tailleLogJeux);
+}
+
 int choixMenu (void)
 {
 	int choix;
@@ -11,6 +20,15 @@ int choixMenu (void)
 		affichageMenu();
 		printf("Erreur lors du choix du menu, veuillez reessayer :");
 		scanf("%d",&choix);
+	}
+	switch (choix)
+	{
+	case 1:
+		test1();
+		break;
+	
+	default:
+		break;
 	}
 	return choix;
 }
@@ -27,3 +45,89 @@ void affichageMenu(void)
 	printf("\t 6. Annulation d'une reservation \n");
 	printf("\t 7. Quitter l'application \n");
 }
+
+int remplirTab(Jeux *tabJeux[],int taillePhyJeux)
+{    
+    Jeux m;
+    int tailleLogJeux=0;
+    FILE *flot;
+    flot=fopen("ficjeux.txt","r");
+    if(flot== NULL)
+    {
+        printf("Erreur de chargement du ficjeux.txt\n");
+        return -1;
+    }
+    m = LireJeux(flot);
+    while(!feof(flot))
+    {    
+        if (tailleLogJeux==taillePhyJeux)
+        {
+        printf("Manque de place dans le tableau de Jeux\n");
+        return -2;
+        }
+        tabJeux[tailleLogJeux] =(Jeux*) malloc(sizeof(Jeux));
+        if (tabJeux[tailleLogJeux]==NULL)
+        {
+            printf("Problème d'allocation du tableau jeux\n");
+            fclose(flot);
+            return -3;
+        }
+        *tabJeux[tailleLogJeux]=m;
+        tailleLogJeux=tailleLogJeux+1;
+        m = LireJeux(flot);
+    }
+    fclose(flot);
+    return tailleLogJeux;
+}
+
+
+Jeux LireJeux (FILE *flot)
+{
+    Jeux a;
+    fscanf(flot,"%d",&a.id);
+    fscanf(flot,"%s",a.type);
+    fscanf(flot,"%d%*c",&a.nbExemplaire);
+    fgets(a.nom,29,flot);
+    a.nom[strlen(a.nom)-1] = '\0';
+    return a;
+
+}
+
+void triTabJeux(Jeux *tabJeux[], int tailleLogJeux)
+{
+    int i, j;
+    Jeux *tmp;
+    for(i=0;i<tailleLogJeux;i++)
+    {
+        for(j=i+1 ; j<tailleLogJeux ; j++)
+        {
+            if((strcmp(tabJeux[j]->type, tabJeux[i]->type))<0)
+            {
+                tmp=copyJeu(tabJeux[i]);
+                tabJeux[i]=tabJeux[j];
+                tabJeux[j]=tmp;
+            }
+        }
+    }
+}
+
+Jeux* copyJeu(Jeux *Jeu)
+{
+    Jeux *copie;
+    copie=(Jeux*) malloc(sizeof(Jeux));
+    copie->id=Jeu->id;
+    strcpy(copie->nom,Jeu->nom);
+    copie->nbExemplaire=Jeu->nbExemplaire;
+    strcpy(copie->type,Jeu->type);
+    return copie;
+}
+
+void afficherJeux( Jeux *tabJeux[], int tailleLogJeux)
+{    
+    printf("idJeux\ttypeJeux\tNombres d'exmplaires\tnomJeux\n");
+    for (int i = 0; i < tailleLogJeux; ++i)
+    {
+        printf("  %d\t%s\t\t\t%d\t\t%s\n",tabJeux[i]->id,tabJeux[i]->type,tabJeux[i]->nbExemplaire,tabJeux[i]->nom);
+    }
+}
+
